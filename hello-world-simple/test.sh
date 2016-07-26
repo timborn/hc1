3b0e4587a340a284007ddb0c995ca19223836645
Tim Born <timborn@deltacfax.com>
2016-07-26 15:20:02 +0100

    make a happy noise when all tests pass

--- hello-world-simple/test.sh
+++ hello-world-simple/test.sh
@@ -9,4 +9,7 @@ curl -sSf ${URL} > /dev/null 2>&1
 [ $? != 0 ] && echo "Unable to view website=${URL}" && exit -1 
 curl -sSf ${URL}/random > /dev/null 2>&1 	
 [ $? != 22 ] && echo "Error handling failed" && exit -1	
+
+# normally I like "silence is golden", but in this case a little reassurance
+echo "All Tests Pass (ATP)"
 exit 0
